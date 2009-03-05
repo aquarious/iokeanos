2c3d781c732022cd746de625c791d9b1471e7ad5
sth4tbtt <sth4tbtt@e7598192-045d-11de-9fdb-3debd5008f28>
2009-03-05 05:54:04 +0000

    Initial import.

---
+++ trunk/ContactSpring/src/com/iokeanos/spring/service/Contact.java
@@ -0,0 +1,14 @@
+package com.iokeanos.spring.service;
+
+public class Contact {
+	int contactId;
+    String firstName;
+    String lastName;
+    public int getContactId() {
+            return contactId;
+    }
+    public void setContactId(int contactId) {
+            this.contactId = contactId;
+    }
+
+}
---
+++ trunk/ContactSpring/src/com/iokeanos/spring/service/impl/ContactDAOImpl.java
@@ -0,0 +1,33 @@
+package com.iokeanos.spring.service.impl;
+
+import java.util.List;
+
+import com.iokeanos.spring.service.Contact;
+import com.iokeanos.spring.service.ContactDAO;
+
+public class ContactDAOImpl implements ContactDAO {
+    public Contact getContact(int contactId) {
+        System.out.println("Inside ContactDAOImpl.getContact()");
+        return null;
+    }
+
+	public void deleteContact(int contactId) {
+		System.out.println("deleteContact(int contactId)");
+		
+	}
+
+	public List getContactList() {
+		System.out.println("getContactList()");
+		return null;
+	}
+
+	public void insertContact(Contact contact) {
+		System.out.println("insertContact(Contact contact)");
+		
+	}
+
+	public void updateContact(Contact contact) {
+		System.out.println("updateContact(Contact contact)");
+		
+	}
+}
