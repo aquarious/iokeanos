8fe9de9785c2346a753f61a467c9bb3a6769183f
sth4tbtt <sth4tbtt@e7598192-045d-11de-9fdb-3debd5008f28>
2009-03-05 05:55:19 +0000

    Initial import.

---
+++ HelloSpring/src/com/iokeanos/spring/HelloWorld.java
@@ -0,0 +1,21 @@
+package com.iokeanos.spring;
+
+
+public class HelloWorld {
+/*	ContactDAO contactDAO;
+    public ContactDAO getContactDAO() {
+        return contactDAO;
+    }
+    public void setContactDAO(ContactDAO contactDAO) {
+        this.contactDAO = contactDAO;
+    }*/
+    public void start() throws Exception {
+        System.out.println("Hello Spring World!! " );
+        //System.out.println(contactDAO.getContactList()  );
+    }
+    public void stop() throws Exception {
+        System.out.println("Goodbye Spring World!!");
+    }
+
+
+}
